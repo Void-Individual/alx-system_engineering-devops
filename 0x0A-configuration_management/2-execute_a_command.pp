5ef0fc718b26bd950baa804b0222c92af113276e
Void-Individual <otayomitv@gmail.com>
2024-03-22 23:47:13 +0100

    set a path

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,7 @@
 # File script to close process killmenow
 
 exec { 'kill_killmenow_process':
+    path        => '/usr/bin',
     command     => 'pkill -f killmenow',
     refreshonly => true,
     subscribe   => Exec['start_killmenow_process'],
