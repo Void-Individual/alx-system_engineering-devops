5d8c96b9d93480f042e5b19ed68dcc794427c463
Void-Individual <otayomitv@gmail.com>
2024-03-22 23:44:04 +0100

    bug fixes

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
 # A file script to install Flask
 
-exec { 'install_flask':
-    command => '/usr/bin/pip3 install flask==2.1.0',
-    path    => ['/usr/bin', '/usr/local/bin'],
-    creates => '/usr/local/lib/python3.8/dist-packages/flask/__init__.py'
+package { 'flask':
+    ensure      => '2.1.0',
+    provider    => 'pip3'
 }
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,7 @@
+# File script to close process killmenow
+
 exec { 'kill_killmenow_process':
     command     => 'pkill -f killmenow',
     refreshonly => true,
-    # Subscribe to a command that starts the process
     subscribe   => Exec['start_killmenow_process'],
 }
