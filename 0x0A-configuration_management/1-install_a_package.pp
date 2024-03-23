b4917d3fc7a99f105e42691ec57f000a31a0065f
Void-Individual <otayomitv@gmail.com>
2024-03-23 15:47:51 +0100

    updated werkzeug download

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -5,3 +5,9 @@ exec { 'install_flask':
     path    => '/usr/bin',
     creates => '/usr/local/lib/python3.8/dist-packages/flask/__init__.py'
 }
+
+exec { 'install_Werkzeug':
+    command => '/usr/bin/pip3 install Werkzeug ==2.1.1'
+    path    => ['/usr/bin', '/usr/local/bin'],
+  creates => '/usr/local/lib/python3.8/dist-packages/werkzeug/__init__.py'
+}
