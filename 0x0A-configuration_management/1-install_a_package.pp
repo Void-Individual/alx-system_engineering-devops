50f35d124dc40193bd37578839fb85ee0aac4efa
Void-Individual <otayomitv@gmail.com>
2024-03-23 15:54:39 +0100

    forgot to add a comma before the path

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -6,8 +6,8 @@ exec { 'install_flask':
     creates => '/usr/local/lib/python3.8/dist-packages/flask/__init__.py'
 }
 
-exec { 'install_Werkzeug':
-    command => '/usr/bin/pip3 install Werkzeug ==2.1.1'
-    path    => ['/usr/bin', '/usr/local/bin'],
-  creates => '/usr/local/lib/python3.8/dist-packages/werkzeug/__init__.py'
+exec { 'install_werkzeug':
+    command => '/usr/bin/pip3 install Werkzeug==2.1.1',
+    path    => '/usr/bin',
+    creates => '/usr/local/lib/python3.8/dist-packages/werkzeug/__init__.py'
 }
