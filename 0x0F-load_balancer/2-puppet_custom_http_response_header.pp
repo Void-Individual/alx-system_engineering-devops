71783a4202e3699832bff0bebf2daa35cc698bbe
Void-Individual <otayomitv@gmail.com>
2024-04-01 18:45:28 +0100

    testing puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,43 +1,21 @@
 # Puppet script to create http header response on a server
 
-# Retrieve the hostname of the machine
-$name = $::hostname
-$config = "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
+exec { 'update':
+    command => '/usr/bin/apt-get update',
+}
 
-    root /;
-    index hello.html;
-
-    server_name _;
-
-    error_page 404 /my_404.html;
-
-    location /redirect_me {
-        return 301 https://github.com/Void-Individual;
-    }
-
-    location / {
-        add_header X-Served-By \"$name\";
-        try_files \$uri \$uri/ =404;
-    }
-}"
 #Install nginx and configure it
 package { 'nginx':
     ensure => installed,
 }
 
-file { 'puppet_index.html':
-    path    => '/puppet_index.html',
-    content => "Hello World!\n",
+file_line {'http_header':
+    path    => '/etc/nginx/nginx.conf',
+    match   => 'http {',
+    line    => "http {\n\tadd_header X-Served-By \"{hostname}\";",
+    require => Package['nginx'],
 }
 
-file { '/etc/nginx/sites-available/default':
-    content => $config,
-}
-
-service { 'nginx':
-    ensure  => running,
-    enable  => true,
-    require => File['/etc/nginx/sites-available/default'],
+exec {'run':
+    command => 'service nginx restart',
 }
