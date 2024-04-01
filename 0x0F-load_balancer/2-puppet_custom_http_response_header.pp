7b14da30199e335f885423fcf676119ad4fb3a1e
Void-Individual <otayomitv@gmail.com>
2024-04-01 18:58:10 +0100

    correction

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,21 +1,26 @@
 # Puppet script to create http header response on a server
 
 exec { 'update':
-    command => '/usr/bin/apt-get update',
+    provider =>
+    command  => 'sudo apt-get -y update',
+    before   => Exec['install nginx],
 }
 
 #Install nginx and configure it
-package { 'nginx':
-    ensure => installed,
+exec { 'install nginx':
+    provider => shell,
+    command  => 'sudo apt-get -y install nginx',
+    before   => 'Exec['add_header'],
 }
 
-file_line {'http_header':
-    path    => '/etc/nginx/nginx.conf',
-    match   => 'http {',
-    line    => "http {\n\tadd_header X-Served-By \"{hostname}\";",
-    require => Package['nginx'],
+Exec { 'add_header':
+    provider    => shell,
+    environment => ["HOST=${hostname}"],
+    command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
+    before      => Exec['restart Nginx'],
 }
 
-exec {'run':
-    command => 'service nginx restart',
+exec { 'restart Nginx':
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }
