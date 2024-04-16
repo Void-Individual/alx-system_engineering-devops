e711fc3d1b00f2e4761221bf378811ba9bfc9bc1
Void-Individual <otayomitv@gmail.com>
2024-04-16 22:32:57 +0100

    this shouldnt work

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,4 +1,18 @@
 # Puppet script to install nginx server
+
+exec { 'update':
+    provider =>
+    command  => 'sudo apt-get -y update',
+    before   => Exec['install nginx],
+}
+
+#Install nginx and configure it
+exec { 'install nginx':
+    provider => shell,
+    command  => 'sudo apt-get -y install nginx',
+}
+
+
 exec {'install':
     provider => shell,
     command  => 'sudo apt-get -y update; sudo apt-get -y install nginx; echo "Hello World!" | sudo tee /puppet_index.html; sudo sed -i s"/server_name _;/root /;\n\tindex puppet_index.;\n\n\tserver_name _;\n\n\trewrite ^\/redirect_me https:\/\/github.com\/otayomitv permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
