f2e41859ef023fdc97623fc687ef69b9362369f8
Void-Individual <otayomitv@gmail.com>
2024-04-23 01:22:17 +0100

    my output doesnt have quotes

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Script to export data to csv format"""
+
+import csv
+import requests
+import sys
+
+if __name__ == "__main__":
+    userId = sys.argv[1]
+
+    url = f"https://jsonplaceholder.typicode.com/users/{userId}"
+    user_info = requests.get(url).json()
+    name = user_info.get("username")
+
+    url = f"https://jsonplaceholder.typicode.com/todos?userId={userId}"
+    all_tasks = requests.get(url).json()
+    data = []
+    for task in all_tasks:
+        status = task.get("completed")
+        title = task.get("title")
+        value = [userId, name, status, title]
+        data.append(value)
+
+    filename = f"{userId}.csv"
+    with open(filename, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(data)
